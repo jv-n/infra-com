bdff400791b67952bff1c8ccd2e44aea2420fcc3
Túlio Carvalho Lira <tulio.lira@outlook.com>
2025-02-05 16:37:26 -0300

    (separando-pastas): arquivos funcionando

--- Entrega_1/cliente/cliente.py
+++ Entrega_1/cliente/cliente.py
@@ -4,8 +4,8 @@ import socket
 SERVER_IP = "127.0.0.1" 
 SERVER_PORT = 12000
 BUFFER_SIZE = 1024
-#FILE_TO_SEND = "data.txt"  # Arquivo a ser enviado (modelo txt)
-FILE_TO_SEND = "data.jpg"  # Arquivo a ser enviado (modelo jpg)
+FILE_TO_SEND = "data.txt"  # Arquivo a ser enviado (modelo txt)
+#FILE_TO_SEND = "data.jpg"  # Arquivo a ser enviado (modelo jpg)
 
 # Criando o socket UDP
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -14,7 +14,7 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_socket.sendto(FILE_TO_SEND.encode(), (SERVER_IP, SERVER_PORT))
 
 # Enviar arquivo em pacotes
-with open(FILE_TO_SEND, "rb") as file:
+with open(f"cliente/{FILE_TO_SEND}", "rb") as file:
     while chunk := file.read(BUFFER_SIZE):
         client_socket.sendto(chunk, (SERVER_IP, SERVER_PORT))
 
@@ -27,7 +27,7 @@ new_file_name = new_file_name.decode()
 print(f"Arquivo será salvo como: {new_file_name}")
 
 # Receber o arquivo renomeado e salvar
-with open(new_file_name, "wb") as file:
+with open(f"cliente/{new_file_name}", "wb") as file:
     while True:
         data, _ = client_socket.recvfrom(BUFFER_SIZE)
         if data == b"END":  # Verifica fim da transmissão
