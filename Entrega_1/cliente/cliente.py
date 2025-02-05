5c2ab14c5e9c9e8fc5582a873f4e43402eae08b8
Túlio Carvalho Lira <tulio.lira@outlook.com>
2025-02-05 16:28:59 -0300

    (separando-pastas): código funcionando

--- Entrega_1/cliente/cliente.py
+++ Entrega_1/cliente/cliente.py
@@ -4,8 +4,8 @@ import socket
 SERVER_IP = "127.0.0.1" 
 SERVER_PORT = 12000
 BUFFER_SIZE = 1024
-FILE_TO_SEND = "./cliente/data.txt"  # Arquivo a ser enviado (modelo txt)
-#FILE_TO_SEND = "./cliente/data.jpg"  # Arquivo a ser enviado (modelo jpg)
+#FILE_TO_SEND = "data.txt"  # Arquivo a ser enviado (modelo txt)
+FILE_TO_SEND = "data.jpg"  # Arquivo a ser enviado (modelo jpg)
 
 # Criando o socket UDP
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -27,7 +27,7 @@ new_file_name = new_file_name.decode()
 print(f"Arquivo será salvo como: {new_file_name}")
 
 # Receber o arquivo renomeado e salvar
-with open(f"./cliente/{new_file_name}", "wb") as file:
+with open(new_file_name, "wb") as file:
     while True:
         data, _ = client_socket.recvfrom(BUFFER_SIZE)
         if data == b"END":  # Verifica fim da transmissão
