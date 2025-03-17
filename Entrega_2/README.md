71b8581f28d352a0cd18c38231f19b663ebdc56c
Túlio Carvalho Lira <tulio.lira@outlook.com>
2025-03-17 20:19:17 -0300

    (feat/entrega-2): adicionei pasta da entrega 2

---
+++ Entrega_2/README.md
@@ -0,0 +1,15 @@
+Entrega 1)
+    
+    1. Abrir terminal na pasta Entrega_1
+    2. Rodar o servidor com 'python ./servidor/servidor.py'
+    3. Abrir outra aba no terminal
+    4. Executar cliente com o comando 'python ./cliente/cliente.py'
+    5. Para testar diferentes tipos de arquivos, alterar a variável FILE_TO_SEND em cliente/cliente.py
+
+Grupo
+- Luiz Gustavo Santa Cruz Silva Segundo  - lgscss
+- Heitor Higino Holanda Cordeiro         - hhhc
+- Túlio Fernando Carvalho de lira        - tfcl
+- João Victor da Silva Nascimento        - jvsn2
+- Felipe Mateus Falcão Barreto           - fmfb
+- Breno Ramos de Souza Gomes             - brsg
---
+++ Entrega_2/cliente/cliente.py
@@ -0,0 +1,39 @@
+import socket
+
+# Configuração do cliente
+SERVER_IP = "127.0.0.1" 
+SERVER_PORT = 12000
+BUFFER_SIZE = 1024
+#FILE_TO_SEND = "data.txt"  # Arquivo a ser enviado (modelo txt)
+FILE_TO_SEND = "data.jpg"  # Arquivo a ser enviado (modelo jpg)
+
+# Criando o socket UDP
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Enviar nome do arquivo
+client_socket.sendto(FILE_TO_SEND.encode(), (SERVER_IP, SERVER_PORT))
+
+# Enviar arquivo em pacotes
+with open(f"cliente/{FILE_TO_SEND}", "rb") as file:
+    while chunk := file.read(BUFFER_SIZE):
+        client_socket.sendto(chunk, (SERVER_IP, SERVER_PORT))
+
+# Enviar sinal de fim
+client_socket.sendto(b"END", (SERVER_IP, SERVER_PORT))
+
+# Receber o novo nome do arquivo
+new_file_name, _ = client_socket.recvfrom(BUFFER_SIZE)
+new_file_name = new_file_name.decode()
+print(f"Arquivo será salvo como: {new_file_name}")
+
+# Receber o arquivo renomeado e salvar
+with open(f"cliente/{new_file_name}", "wb") as file:
+    while True:
+        data, _ = client_socket.recvfrom(BUFFER_SIZE)
+        if data == b"END":  # Verifica fim da transmissão
+            break
+        file.write(data)
+
+print(f"Arquivo '{new_file_name}' recebido com sucesso!")
+
+client_socket.close()
