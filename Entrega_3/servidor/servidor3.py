c0a031a2fd022ff8875e41eb03f22381903db91d
Luiz Gustavo <148971633+lzgustavo13@users.noreply.github.com>
2025-04-05 11:25:41 -0300

    Create servidor3.py

---
+++ Entrega_3/servidor/servidor3.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+import time
+
+clients = {}
+users_online = set()
+friendships = {}
+groups = {}
+
+def commandrcv(address, server_socket):
+    while True:
+        try:
+            data, addr = server_socket.recvfrom(1024)
+            message = data.decode()
+            if not message:
+                continue
+
+            command_parts = message.strip().split(" ")
+            command = command_parts[0]
+            username = clients.get(addr, None)
+
+            if command == "login":
+                username = command_parts[1]
+                clients[addr] = username
+                users_online.add(username)
+                server_socket.sendto(f"Usuário {username} logado com sucesso.".encode(), addr)
+
+            elif command == "login":
+                if username:
+                    users_online.discard(username)
+                    clients.pop(addr)
+                    server_socket.sendto(f"Logout do usuário {username} realizado com sucesso".encode(), addr)
